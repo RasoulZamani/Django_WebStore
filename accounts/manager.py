94bfe2cd4561d978edd7fa83cb4cecb1330502b3
RasoulZamani <r.zamani72@gmail.com>
2023-03-18 18:15:37 +0330

    writing custom user

---
+++ accounts/manager.py
@@ -0,0 +1,35 @@
+from django.contrib.auth.models import BaseUserManager
+
+class MyUserManager(BaseUserManager):
+    """customized manager"""
+    
+    def create_user(self, phone, email, password):
+        
+        if not phone:
+            raise ValueError("user must have phone number")
+        
+        if not email:
+            raise ValueError("user must have email address")
+        
+        user = self.model(phone=phone, email=self.normalize_email(email))
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
+    def create_supperuser(self, phone, email, password):
+        user = self.create_user( phone, email, password)
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
+    
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from manager import MyUserManager
 
-# Create your models here.
+class MyUser(AbstractBaseUser):
+    """ customized user class"""
+    email = models.EmailField(max_length=255, unique=True)
+    phone = models.CharField(max_length=11, unique=True)
+    is_active = models.BooleanField(default=True)
+    is_admin  = models.BooleanField(default=False)
+    
+    USERNAME_FIELD = 'phone' 
+    REQUIRED_FIELDS = ['email']
+    
+    objects = MyUserManager()
+    
+    def __str__(self) -> str:
+        return self.email
+    
+    def has_perm(self, perm, obj=None):
+        return True
+    
+    def has_module_perms(self, app_label):
+        return True
+    
+    @property
+    def is_staff(self):
+        return self.is_admin
