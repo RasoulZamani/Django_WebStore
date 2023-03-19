1b02fe1344ab0ff346d6b9be4dc6bb52f69f5803
RasoulZamani <r.zamani72@gmail.com>
2023-03-19 15:37:41 +0330

    a little style by css

--- store/settings.py
+++ store/settings.py
@@ -13,8 +13,9 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-TEMP_DIR = BASE_DIR.joinpath('templates')
+BASE_DIR   = Path(__file__).resolve().parent.parent
+TEMP_DIR   = BASE_DIR.joinpath('templates')
+STATIC_DIR = BASE_DIR.joinpath('static')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
@@ -118,7 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATICFILES_DIRS = [ STATIC_DIR ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
